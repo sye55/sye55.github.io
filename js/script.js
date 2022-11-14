0bc2c5d40e71f4310f84219f0e0a00cbba09f709
Simon Esslemont <simon.esslemont.abz@outlook.com>
2022-11-14 02:53:59 +0000

    popups added to features

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([57.155, -2.123], 12);
+const d = document.getElementById("map");
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,13 +11,33 @@ async function getStreets() {
     const resp = await fetch('./js/streets.geojson');
     const data = await resp.json();
     
-    L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+        onEachFeature: onEachFeature
+    }).addTo(map);
     map.fitBounds(map.getBounds());
     map.setMaxBounds([
         [57.30, -1.8],
         [57.0, -2.4]
     ]);
+ 
     
+    function onEachFeature(feature, layer){
+
+        layer.bindPopup(feature.properties.name, {className: "custom-popup", autoPan: false});
+        layer.setStyle({color: 'blue', weight: 8});
+        layer.on('mousemove mouseover', function(e) {
+            let ev = e.originalEvent;
+            let divBox = d.getBoundingClientRect();
+            let p = L.point([ev.clientX - divBox.left, ev.clientY - divBox.top]); // cursor
+            let latlng = map.containerPointToLatLng(p);
+            layer.setStyle({color: 'yellow'});
+            layer.openPopup(latlng);
+        });
+        layer.on('mouseout', function(e) {
+            layer.setStyle({color: 'blue'});
+            layer.closePopup();
+        });
+    }
 }
 var streets = getStreets();
 
